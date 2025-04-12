8a0014ebc277476b40b1859caeaa0f9205dbd50e
Felipe Jimenez <jfjimenezsalazar@gmail.com>
2025-04-12 13:21:30 -0500

    se quita la verificacion y autorizacion  a obtener productos porque es una ruta pública

--- backend/src/routes/productRoutes.js
+++ backend/src/routes/productRoutes.js
@@ -6,7 +6,7 @@ const { verifyToken, verifyAuthorization } = require('../middleware/Auth');
 
 //rutas para los productos
 router.post('/producto', upload.single('image'), verifyToken, verifyAuthorization, productController.agregarProducto);  // Para agregar un nuevo producto
-router.get('/productos', verifyToken, verifyAuthorization, productController.listarProductos);  // Para listar todos los productos
+router.get('/productos', productController.listarProductos);  // Para listar todos los productos
 // router.get('/producto/:id', productController.listarProductoPorId);  // Para listar un producto por ID
 // router.put('/producto/:id', productController.actualizarProducto);  // Para actualizar un producto por ID
 router.delete('/producto/:id', verifyToken, verifyAuthorization, productController.eliminarProducto);  // Para eliminar un producto por ID
